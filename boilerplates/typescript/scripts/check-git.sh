e099a8ce1b270d02d1b258bbb523b43e8508ab51
agallio <agalliosamai@gmail.com>
2021-03-22 13:00:42 +0700

    feat(commitlint): modify hooks from package.json to specific folder (typescript)

--- boilerplates/typescript/scripts/check-git.sh
+++ boilerplates/typescript/scripts/check-git.sh
@@ -1,7 +1,8 @@
 if git rev-parse --is-inside-git-dir > /dev/null 2>&1; then
   echo "Git has been initialized. I won't do anything."
 else
-  echo "Git initializing"
+  echo "Git initializing. Preparing husky and eslint..."
   git init
   yarn husky install
+  npx husky add .husky/pre-commit 'yarn lint'
 fi
