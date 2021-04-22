06fddf0aad024031eb8f21a9529691886db8c7ba
agallio <agalliosamai@gmail.com>
2021-04-22 11:25:06 +0700

    chore: update package version & add comment to auth middleware (javascript)

--- boilerplates/javascript/middleware/authenticated.js
+++ boilerplates/javascript/middleware/authenticated.js
@@ -1,7 +1,5 @@
 // Authentication middleware
-const Authenticated = ({ app, store }) => {
+export default function Authenticated({ app, store }) {
   // If the user hasn't logged in, redirect to login page.
   if (!store.state.oauth.accessToken) app.$login()
 }
-
-export default Authenticated
